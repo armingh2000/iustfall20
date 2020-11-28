24e23973afd812b40cd60b3237a119beea18af49
Armin Gholampoor <armingh379@gmail.com>
2020-11-28 16:42:51 -0500

    q2 pass

--- HW3/q2/server.c
+++ HW3/q2/server.c
@@ -4,63 +4,72 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <pthread.h>
 
 
 // in your browser type: http://localhost:8090
 // IF error: address in use then change the PORT number
 #define PORT 8090
 
+void * worker_func(void * _arg) {
+	int * new_socket_ptr = (int *)_arg;
+	int new_socket = * new_socket_ptr;
+	long valread;
+	char *hello = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
+
+	char buffer[30000] = {0};
+	valread = read( new_socket , buffer, 30000);
+	printf("%s\n",buffer );
+	// uncomment following line and connect many clients
+	sleep(5);
+	write(new_socket , hello , strlen(hello));
+	printf("-------------Hello message sent---------------\n");
+	close(new_socket);
+}
 
 int main(int argc, char const *argv[])
 {
-    int server_fd, new_socket; long valread;
-    struct sockaddr_in address;
-    int addrlen = sizeof(address);
-    
-    char *hello = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
-    
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-    {
-        perror("In socket");
-        exit(EXIT_FAILURE);
-    }
-    
+	int server_fd, new_socket; long valread;
+	struct sockaddr_in address;
+	int addrlen = sizeof(address);
 
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
-    
-    memset(address.sin_zero, '\0', sizeof address.sin_zero);
-    
-    
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0)
-    {
-        perror("In bind");
-        exit(EXIT_FAILURE);
-    }
-    if (listen(server_fd, 10) < 0)
-    {
-        perror("In listen");
-        exit(EXIT_FAILURE);
-    }
-    while(1)
-    {
-        printf("\n+++++++ Waiting for new connection ++++++++\n\n");
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
-        {
-            perror("In accept");
-            exit(EXIT_FAILURE);
-        }
-        
-        char buffer[30000] = {0};
-        valread = read( new_socket , buffer, 30000);
-        printf("%s\n",buffer );
-        // uncomment following line and connect many clients
-        // sleep(5);
-        write(new_socket , hello , strlen(hello));
-        printf("-------------Hello message sent---------------");
-        close(new_socket);
-    }
-    return 0;
+
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+	{
+		perror("In socket");
+		exit(EXIT_FAILURE);
+	}
+
+
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons( PORT );
+
+	memset(address.sin_zero, '\0', sizeof address.sin_zero);
+
+
+	if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0)
+	{
+		perror("In bind");
+		exit(EXIT_FAILURE);
+	}
+	if (listen(server_fd, 10) < 0)
+	{
+		perror("In listen");
+		exit(EXIT_FAILURE);
+	}
+	while(1)
+	{
+		printf("\n+++++++ Waiting for new connection ++++++++\n\n");
+		if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
+		{
+			perror("In accept");
+			exit(EXIT_FAILURE);
+		}
+		pthread_t * thread;
+		pthread_create(thread, NULL, worker_func, (void*)&new_socket);
+
+	}
+	return 0;
 }
 
